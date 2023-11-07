8b5a7c5e8f35b07668b1d0b3304df2127f90dc8d
Mihael <mihael.belko@windowslive.com>
2023-11-07 18:42:28 +0100

    New comparing objects implementation

--- src/main/java/leapwise/soft/expressionevaluator/algorithm/helper/NumericStringCheckerImpl.java
+++ src/main/java/leapwise/soft/expressionevaluator/algorithm/helper/NumericStringCheckerImpl.java
@@ -1,10 +1,17 @@
 package leapwise.soft.expressionevaluator.algorithm.helper;
 
+import leapwise.soft.expressionevaluator.algorithm.tree.nodes.Node;
 import leapwise.soft.expressionevaluator.exception.NonNumericStringException;
 
 import static leapwise.soft.expressionevaluator.exception.algorithm.AlgorithmExceptionMessage.NON_NUMERIC_STRING;
 
 public class NumericStringCheckerImpl {
+
+  public static boolean areNumericEquals(Node n1, Node n2) {
+    Double num1 = NumericStringCheckerImpl.checkIfNumeric(n1.getNodeValue());
+    Double num2 = NumericStringCheckerImpl.checkIfNumeric(n2.getNodeValue());
+    return num1.equals(num2);
+  }
 
   public static Double checkIfNumeric(String input) {
     try {
